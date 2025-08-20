6136f4ddead66f0ffa6c37559cf327c5cce82fd4
Christopher Chou <emu4yolife@gmail.com>
2025-08-20 22:02:59 +0000

    Implement GQA

--- partitioning/flax/attention.py
+++ partitioning/flax/attention.py
@@ -10,7 +10,7 @@ from jax.experimental import pjit
 
 from partitioning.flax.mlp import ColumnParallelLinear, RowParallelLinear
 
-# Megatron-style Attention
+# Megatron-style Multi-head Attention
 class MegatronAttention(nnx.Module):
     def __init__(self, hidden_dim, head_dim, num_heads, rngs):
         self.wqkv = ColumnParallelLinear(hidden_dim, head_dim * num_heads * 3, rngs)
@@ -45,10 +45,50 @@ class MegatronAttention(nnx.Module):
             outputs = self.wo(values)
 
         return outputs
+
+class GroupedQueryAttention(nnx.Module):
+    def __init__(self, hidden_dim, head_dim, num_heads, num_kv_heads, rngs):
+        self.wq = ColumnParallelLinear(hidden_dim, hidden_dim, rngs)
+
+        kv_size = hidden_dim * num_kv_heads // num_heads
+        self.wk = ColumnParallelLinear(hidden_dim, kv_size, rngs)
+        self.wv = ColumnParallelLinear(hidden_dim, kv_size, rngs)
+        self.wo = RowParallelLinear(kv_size, hidden_dim, rngs)
+        self.num_heads = num_heads
+        self.num_kv_heads = num_kv_heads
+        self.num_head_groups = num_heads // num_kv_heads
+        self.head_dim = head_dim
     
-@nnx.jit(static_argnames=("hidden_dim", "head_dim", "num_heads"))
-def create_sharded_attention(hidden_dim, head_dim, num_heads):
-    attention = MegatronAttention(hidden_dim, head_dim, num_heads, nnx.Rngs(0))
+    def __call__(self, x):
+        # [b, s, d] -> [b, s,]
+        q, k, v = self.wq(x), self.wk(x), self.wv(x)
+        
+        # Difference now is that the number of kv heads is different between q and k, v
+        q = rearrange(q, "b s (h d_head)->b h s d_head", h=self.num_heads)
+        k = rearrange(k, "b s (h d_head)->b h d_head s", h=self.num_kv_heads)
+        v = rearrange(v, "b s (h d_head)->b h s d_head", h=self.num_kv_heads)
+
+        # Rearrange into groups of heads
+        # h in this case will become the num_kv_heads and g is the number of groups
+        q = rearrange(q, "b (h g) s d_head -> b g h s d_head", g=self.num_head_groups)
+
+        # Omit the g in the output will sum across that dimension. This means that for each kv head
+        # we sum across the group of query heads.
+        scores = jnp.einsum("b g h q d,b h d k->b h q k", q, k) / jnp.sqrt(self.head_dim)
+        scores = jax.nn.softmax(scores, axis=-1)
+        values = jnp.einsum("bhqk,bhkd->bhqd", scores, v)
+        values = rearrange(values, "b h q d->b q (h d)", h=self.num_kv_heads)
+        outputs = self.wo(values)
+        return outputs
+    
+@nnx.jit(static_argnames=("hidden_dim", "head_dim", "num_heads", "attention_type"))
+def create_sharded_attention(hidden_dim, head_dim, num_heads, attention_type):
+    if attention_type == "megatron":
+        attention = MegatronAttention(hidden_dim, head_dim, num_heads, nnx.Rngs(0))
+    elif attention_type == "gqa":
+        # 4 query heads per 1 key and value head
+        attention = GroupedQueryAttention(hidden_dim, head_dim, num_heads, num_heads // 4, nnx.Rngs(0))
+
     state = nnx.state(attention)
     pspec = nnx.get_partition_spec(state)
     sharded_state = jax.lax.with_sharding_constraint(state, pspec)
@@ -64,7 +104,7 @@ def megatron_attention_parallelism():
     head_dim = hidden_dim // num_heads
     with jax.profiler.trace('/tmp/jax-trace', create_perfetto_link=True, create_perfetto_trace=True):
         with mesh:
-            attention = create_sharded_attention(hidden_dim, head_dim, num_heads)
+            attention = create_sharded_attention(hidden_dim, head_dim, num_heads, "gqa")
             x = jax.random.normal(jax.random.PRNGKey(0), (1, 16, hidden_dim))
             # Compile the forward with pjit and print compiler IR text
             def forward(inp):
