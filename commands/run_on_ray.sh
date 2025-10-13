35f8b573e67dfb3fa2c1f1fae99dd4a8c2b07e72
Christopher Chou <christopherchou@berkeley.edu>
2025-10-13 10:48:10 -0700

    Change to src setup

--- commands/run_on_ray.sh
+++
@@ -1,4 +0,0 @@
-RAY_ADDRESS=http://localhost:8268 ray job submit \
-  --address="http://<head-node>:8265" \
-  --runtime-env-json='{"working_dir": "./", "env_vars": {"TPU_MIN_LOG_LEVEL": "3", "XLA_FLAGS": "--xla_hlo_profile"}, "pip": ["einops", "pytest"]}' \
-  -- pytest tests/partitioning/flax/test_attention.py
